546f02ae27d699fbd71dac9989831e3a04bedbcd
Wagner Pupo <wagner-wen@hotmail.com>
2022-07-19 10:13:37 -0300

    Projeto Criado

---
+++ src/test/java/com/wagnerpupo/CursoMC/CursoMcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wagnerpupo.CursoMC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoMcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
