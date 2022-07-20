294f55a6517d6570fdaee5f1685daa83538ea4d8
Wagner Pupo <wagner-wen@hotmail.com>
2022-07-20 12:52:42 -0300

    Criando repository e services para categoria

--- src/main/java/com/wagnerpupo/CursoMC/resources/CategoriaResource.java
+++ src/main/java/com/wagnerpupo/CursoMC/resources/CategoriaResource.java
@@ -1,28 +1,27 @@
 package com.wagnerpupo.CursoMC.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.wagnerpupo.CursoMC.domain.Categoria;
+import com.wagnerpupo.CursoMC.services.CategoriaService;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
-	@RequestMapping(method=RequestMethod.GET)
-	public List<Categoria> listar() {
+	@Autowired
+	private CategoriaService service;
 	
-		Categoria cat1 = new Categoria (1,"informatica");
-		Categoria cat2 = new Categoria (2,"escritorio");
-		
-		List<Categoria> lista = new ArrayList<>();
-		lista.add(cat1);
-		lista.add(cat2);
-		
-		return lista;
+	@RequestMapping(value="/(id)", method=RequestMethod.GET)
+	public ResponseEntity<?> find (@PathVariable Integer id ) {
+	
+		Categoria obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
+
 	}
 }
---
+++ src/main/java/com/wagnerpupo/CursoMC/services/CategoriaService.java
@@ -0,0 +1,26 @@
+package com.wagnerpupo.CursoMC.services;
+
+
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.wagnerpupo.CursoMC.domain.Categoria;
+import com.wagnerpupo.CursoMC.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+		
+	}
+	
+}
