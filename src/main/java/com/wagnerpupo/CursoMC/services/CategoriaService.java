121feffebebfb99eff68563255547e88e50a4a98
Wagner Pupo <wagner-wen@hotmail.com>
2022-07-22 16:29:08 -0300

    Ajuste no endpoint /categoria /id

--- src/main/java/com/wagnerpupo/CursoMC/services/CategoriaService.java
+++ src/main/java/com/wagnerpupo/CursoMC/services/CategoriaService.java
@@ -4,6 +4,7 @@ package com.wagnerpupo.CursoMC.services;
 
 import java.util.Optional;
 
+import org.hibernate.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,10 +18,11 @@ public class CategoriaService {
 	private CategoriaRepository repo;
 	
 	public Categoria buscar(Integer id) {
-		
-		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
-		
-	}
+		 Optional<Categoria> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		 "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName(), null));
+		} 
+
+
 	
 }
