2f6a9936b51b00ecb782fdaed12a26568487504c
Wagner Pupo <wagner-wen@hotmail.com>
2022-07-22 15:04:15 -0300

    Criando estanciacao

--- src/main/java/com/wagnerpupo/CursoMC/CursoMcApplication.java
+++ src/main/java/com/wagnerpupo/CursoMC/CursoMcApplication.java
@@ -1,13 +1,32 @@
 package com.wagnerpupo.CursoMC;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursoMcApplication {
+import com.wagnerpupo.CursoMC.domain.Categoria;
+import com.wagnerpupo.CursoMC.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursoMcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoMcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria (null,"Informatica");
+		Categoria cat2 = new Categoria (null,"Escritorio");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 	}
 
 }
--- src/main/java/com/wagnerpupo/CursoMC/resources/CategoriaResource.java
+++ src/main/java/com/wagnerpupo/CursoMC/resources/CategoriaResource.java
@@ -17,7 +17,7 @@ public class CategoriaResource {
 	@Autowired
 	private CategoriaService service;
 	
-	@RequestMapping(value="/(id)", method=RequestMethod.GET)
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
 	public ResponseEntity<?> find (@PathVariable Integer id ) {
 	
 		Categoria obj = service.buscar(id);
