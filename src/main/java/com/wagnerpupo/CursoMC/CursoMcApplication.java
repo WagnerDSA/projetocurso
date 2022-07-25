10b7efd1ebc32fa5226f4d8ee1fcfab86e06f643
Wagner Pupo <wagner-wen@hotmail.com>
2022-07-25 11:12:26 -0300

    cliente/tipocliente/telefone e endercos

--- src/main/java/com/wagnerpupo/CursoMC/CursoMcApplication.java
+++ src/main/java/com/wagnerpupo/CursoMC/CursoMcApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.wagnerpupo.CursoMC.domain.Categoria;
 import com.wagnerpupo.CursoMC.domain.Cidade;
+import com.wagnerpupo.CursoMC.domain.Cliente;
+import com.wagnerpupo.CursoMC.domain.Endereco;
 import com.wagnerpupo.CursoMC.domain.Estado;
 import com.wagnerpupo.CursoMC.domain.Produto;
+import com.wagnerpupo.CursoMC.domain.enums.TipoCliente;
 import com.wagnerpupo.CursoMC.repositories.CategoriaRepository;
 import com.wagnerpupo.CursoMC.repositories.CidadeRepository;
+import com.wagnerpupo.CursoMC.repositories.ClienteRepository;
+import com.wagnerpupo.CursoMC.repositories.EnderecoRepository;
 import com.wagnerpupo.CursoMC.repositories.EstadoRepository;
 import com.wagnerpupo.CursoMC.repositories.ProdutoRepository;
 
@@ -27,6 +32,10 @@ public class CursoMcApplication implements CommandLineRunner {
 	private CidadeRepository cidadeRepository;
 	@Autowired
 	private EstadoRepository estadoRepository;
+	@Autowired
+	private ClienteRepository clienteRepository;
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoMcApplication.class, args);
@@ -65,7 +74,16 @@ public class CursoMcApplication implements CommandLineRunner {
 		estadoRepository.saveAll(Arrays.asList(est1,est2));
 		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
 		
+		Cliente cli1 = new Cliente(null, "Maria Valda", "MariaValda@gmail.com","3637114712541",TipoCliente.PESSOAFISICA);
+		cli1.getTelefone().addAll(Arrays.asList("32232322","25252222"));
 		
+		Endereco e1= new Endereco (null,"Rua Flores","300","ap403","Jardim","382200834", cli1,c1);
+		Endereco e2= new Endereco (null,"Avenida Matos","105","sala 8000","Centro","382202524", cli1,c2);
+		
+		cli1.getEnderecos().addAll(Arrays.asList(e1,e2));
+		
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1,e2));
 	}
 
 }
---
+++ src/main/java/com/wagnerpupo/CursoMC/domain/Cliente.java
@@ -0,0 +1,125 @@
+package com.wagnerpupo.CursoMC.domain;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+
+import javax.persistence.CollectionTable;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import com.wagnerpupo.CursoMC.domain.enums.TipoCliente;
+
+@Entity
+public class Cliente implements Serializable  {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	private String nome ;
+	private String email;
+	private String cpfOucpnj;
+	private Integer tipo ;
+	
+	@OneToMany(mappedBy="cliente")
+	private List<Endereco> enderecos = new ArrayList<>();
+	
+	@ElementCollection
+	@CollectionTable(name="TELEFONE")
+	private Set<String> telefone = new HashSet<>();
+	
+	public Cliente ( ) {
+		
+	}
+
+	public Cliente(Integer id, String nome, String email, String cpfOucpnj, TipoCliente tipo) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+		this.cpfOucpnj = cpfOucpnj;
+		this.tipo = tipo.getCod();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getCpfOucpnj() {
+		return cpfOucpnj;
+	}
+
+	public void setCpfOucpnj(String cpfOucpnj) {
+		this.cpfOucpnj = cpfOucpnj;
+	}
+
+	public TipoCliente getTipo() {
+		return TipoCliente.toEnum(tipo);
+	}
+
+	public void setTipo(TipoCliente tipo) {
+		this.tipo = tipo.getCod();
+	}
+
+	public List<Endereco> getEnderecos() {
+		return enderecos;
+	}
+
+	public void setEnderecos(List<Endereco> enderecos) {
+		this.enderecos = enderecos;
+	}
+
+	public Set<String> getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(Set<String> telefone) {
+		this.telefone = telefone;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+}
