00e5942a8debce93173a5e18deb0e035606ee81f
Wagner Pupo <wagner-wen@hotmail.com>
2022-07-26 16:50:16 -0300

    atualizacao jsonignore

--- src/main/java/com/wagnerpupo/CursoMC/domain/Pagamento.java
+++ src/main/java/com/wagnerpupo/CursoMC/domain/Pagamento.java
@@ -11,7 +11,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.wagnerpupo.CursoMC.domain.enums.EstadoPagamento;
 
 @Entity
@@ -22,8 +22,9 @@ public abstract class Pagamento implements Serializable  {
 	@Id
 	private Integer id;
 	private Integer estado;
-
-	@JsonBackReference
+	
+	
+	@JsonIgnore
 	@OneToOne
 	@JoinColumn(name="pedido_id")
 	@MapsId
--- src/main/java/com/wagnerpupo/CursoMC/domain/Pedido.java
+++ src/main/java/com/wagnerpupo/CursoMC/domain/Pedido.java
@@ -16,7 +16,6 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
 public class Pedido implements Serializable  {
@@ -27,11 +26,9 @@ public class Pedido implements Serializable  {
 	private Integer id;
 	private Date instante;
 	
-	@JsonManagedReference
 	@OneToOne(cascade=CascadeType.ALL,mappedBy = "pedido")
 	private Pagamento pagamento;
 	
-	@JsonManagedReference
 	@ManyToOne
 	@JoinColumn(name="cliente_id")
 	private Cliente cliente;
