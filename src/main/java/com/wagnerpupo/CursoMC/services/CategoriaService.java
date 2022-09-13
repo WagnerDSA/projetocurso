b90ace8733f10e39c8d4aa7e5d50d68f08f2fe9b
Wagner Pupo <91687321+WagnerDSA@users.noreply.github.com>
2022-09-13 11:43:40 -0300

    Atualizando categoria com PUT
    Atualizando CAT

--- src/main/java/com/wagnerpupo/CursoMC/services/CategoriaService.java
+++ src/main/java/com/wagnerpupo/CursoMC/services/CategoriaService.java
@@ -17,7 +17,7 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 	
-	public Categoria buscar(Integer id) {
+	public Categoria find (Integer id) {
 		 Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 		 "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName(), null));
@@ -28,5 +28,9 @@ public Categoria insert (Categoria obj){
 	return repo.save(obj);
 
 }
+public Categoria update (Categoria obj){
+
+	find(obj.getId());
+	return repo.save(obj);
 	
 }
